23b7c8fe271ba5abfc464ac781d8127d71cf54d7
Muhammad Hasan Nizami <mn08065@users.noreply.github.com>
2025-09-24 12:19:54 +0500

    todo 3.2 done

--- LibraryManagementSystem/Domain/Services/LoanService.cs
+++ LibraryManagementSystem/Domain/Services/LoanService.cs
@@ -28,12 +28,15 @@ namespace LibraryManagementSystem.Domain.Services
                 throw new InvalidOperationException("The book is not available for loan.");
             }
             // TODO: 3.2 Check if member can borrow. If not, throw an InvalidOperationException with the message "The member has reached their borrowing limit."
-
+            if (!member.CanBorrow())
+            {
+                throw new InvalidOperationException( "The member has reached their borrowing limit.");
+            }
             // TODO: 3.3 Finally call the RecordLoan method on the member object and pass the loan object as an argument
 
-            // DO NOT MODIFY BELOW THIS LINE
+                // DO NOT MODIFY BELOW THIS LINE
 
-            return loan;
+                return loan;
         }
 
         public void ReturnBook(Loan loan)
